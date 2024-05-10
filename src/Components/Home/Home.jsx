202496a721df51122b5d65e4160ffde764ccee51
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-05-10 12:40:13 +0600

    Added nav bar home page and footer

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+
+import { motion } from "framer-motion";
+import Loader from "../Loader/Loader";
+
+
+
+
+import { Typewriter } from "react-simple-typewriter";
+
+const pageVariants = {
+    initial: {
+        opacity: 0,
+        x: "-100vw"
+    },
+    in: {
+        opacity: 1,
+        x: 0
+    },
+    out: {
+        opacity: 0,
+        x: "100vw"
+    }
+};
+
+
+
+const pageTransition = {
+    type: "tween",
+    ease: "easeInOut",
+    duration: 0.9
+};
+
+
+
+const Home = () => {
+
+   
+
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        document.title = "DinerZZ|Home";
+    }, [])
+
+    useEffect(() => {
+        const delay = setTimeout(() => {
+            setLoading(false);
+        }, 1000)
+        return () => clearTimeout(delay);
+    }, [])
+
+    return (
+        <div>
+            {loading && <Loader></Loader>}
+            {!loading &&
+
+                <motion.div
+
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={pageVariants}
+                    transition={pageTransition}
+
+
+                >
+
+
+                    <div className="max-w-6xl mx-auto">
+
+                        <h1 className="text-3xl font-play-fare font-bold text-center mt-9">
+
+
+                            <Typewriter
+                                loop
+                                cursor
+                                cursorStyle='_'
+                                typeSpeed={70}
+                                deleteSpeed={50}
+                                delaySpeed={2000}
+                                words={['Tourist Spots🗺']}
+                            />
+
+                        </h1>
+                        <div className="divider"></div>
+                        <p className="text-xl font-play-fare text-gray-400 text-center mt-4 mb-5">A tourist spot is a location or destination that <br /> attracts visitors from around the world due to its unique features, <br /> cultural significance, natural beauty, historical importance, or recreational opportunities. </p>
+
+
+
+
+                    </div>
+
+                </motion.div>
+
+            }
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root.jsx';
+import ErrorPage from './Components/ErrorPage/ErrorPage.jsx';
+import Home from './Components/Home/Home.jsx';
+import AuthProvider from './Components/Authprovider/AuthProvider.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+
   </React.StrictMode>,
 )
